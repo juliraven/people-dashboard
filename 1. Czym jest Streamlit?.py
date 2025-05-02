6b12ae981b6d1603b0590144097f8613aec7ccf1
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 01:01:34 +0200

    Update 1. Czym jest Streamlit?.py

--- 1. Czym jest Streamlit?.py
+++ 1. Czym jest Streamlit?.py
@@ -9,12 +9,19 @@ st.markdown(
     Aplikacje można tworzyć wyłącznie z użyciem Pythona i bez konieczności używania innych technologii, takich jak JavaScript, HTML, CSS.
     Dokumentacja dostępna jest na stronie: 
     <a href="https://docs.streamlit.io" style="color:purple; font-weight:bold;">dokumentacja</a>.
-    Utworzoną aplikację można w łatwy sposób wdrożyć dzięki <a href="https://docs.streamlit.io/" style="color:purple; font-weight:bold;">chmurze</a> za pomocą kilku kliknięć.
     </p>
     ''',
     unsafe_allow_html=True
 )
 
+st.markdown(
+    '''
+    <p>
+    Utworzoną aplikację można w łatwy sposób wdrożyć dzięki <a href="https://docs.streamlit.io/" style="color:purple; font-weight:bold;">chmurze</a> za pomocą kilku kliknięć.
+    </p>
+    ''',
+    unsafe_allow_html=True
+)
 
 st.sidebar.image("logo.png", use_container_width=True)
 
