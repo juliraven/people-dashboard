41bf27ce04415e87f9df8d9c9b92ce4fe72192ea
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 00:46:47 +0200

    Update 1. Czym jest Streamlit?.py

--- 1. Czym jest Streamlit?.py
+++ 1. Czym jest Streamlit?.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 st.title("Czym jest Streamlit?")
@@ -13,5 +12,47 @@ st.markdown(
     Utworzoną aplikację można w łatwy sposób wdrożyć dzięki <a href="https://docs.streamlit.io/" style="color:purple; font-weight:bold;">chmurze</a> za pomocą kilku kliknięć.
     </p>
     ''',
+    unsafe_allow_html=True
+)
+
+from streamlit_extras.app_logo import add_logo
+
+st.set_page_config(page_title="Analiza sentymentu", page_icon="🎥", layout="wide")
+
+add_logo('logo.png', height=350)
+
+st.sidebar.markdown(
+    """
+    <style>
+        [data-testid="stSidebar"] {
+            padding-top: 0px;
+            padding: 10px;
+            font-family: sans-serif;
+            font-size: 18px;
+        }
+
+        [data-testid="stSidebarHeader"] {
+            height: 30px;
+            padding: 5px 10px; 
+            margin: 0; 
+            display: flex; 
+            align-items: center;
+            justify-content: center; 
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"] {
+        width: 350px;  /* Ustaw stałą szerokość */
+        min-width: 350px;  /* Minimalna szerokość */
+        max-width: 350px;  /* Maksymalna szerokość */
+    }
+    </style>
+    """,
     unsafe_allow_html=True
 )
