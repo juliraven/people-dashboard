860fa3815dbee2ae506e2d219fc1dd4cd7a556d6
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 00:44:20 +0200

    Update 1. Czym jest Streamlit?.py

--- 1. Czym jest Streamlit?.py
+++ 1. Czym jest Streamlit?.py
@@ -6,11 +6,11 @@ st.title("Czym jest Streamlit?")
 st.markdown(
     '''
     <p>
-    <a href="https://streamlit.io" style="color:red; font-weight:bold;">Streamlit</a> umożliwia tworzenie interaktywnych aplikacji internetowych opartych na danych.
+    <span style="color:red; font-weight:bold;">Streamlit</span> umożliwia tworzenie interaktywnych aplikacji internetowych opartych na danych.
     Aplikacje można tworzyć wyłącznie z użyciem Pythona i bez konieczności używania innych technologii, takich jak JavaScript, HTML, CSS.
     Dokumentacja dostępna jest na stronie: 
-    <a href="https://docs.streamlit.io" target="_blank">dokumentacja</a>.
-    Utworzoną aplikację można w łatwy sposób wdrożyć dzięki chmurze za pomocą kilku kliknięć.
+    <a href="https://docs.streamlit.io" style="color:purple; font-weight:bold;">dokumentacja</a>.
+    Utworzoną aplikację można w łatwy sposób wdrożyć dzięki <a href="https://docs.streamlit.io/" style="color:purple; font-weight:bold;">chmurze</a> za pomocą kilku kliknięć.
     </p>
     ''',
     unsafe_allow_html=True
