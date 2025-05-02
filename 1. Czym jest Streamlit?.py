6792d9cd3221177832b133f0a98798746a032378
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 00:39:15 +0200

    Update 1. Czym jest Streamlit?.py

--- 1. Czym jest Streamlit?.py
+++ 1. Czym jest Streamlit?.py
@@ -1,13 +1,8 @@
 
 import streamlit as st
 
-st.title("Instrukcja uruchomienia środowiska i Streamlit")
+st.title("Czym jest Streamlit?")
 
 st.markdown("""
-## **Aktywuj nowo utworzone środowisko**
-
-Aby skorzystać ze środowiska conda, które właśnie stworzyliśmy i które nazwaliśmy `stenv`, wprowadź następujące polecenie w linii poleceń:
-
-```bash
-conda activate stenv
+Streamlit umożliwia tworzenie interaktywnych aplikacji internetowych opartych na danych. Aplikacje można tworzyć wyłącznie z użyciem Pythona i bez konieczności używania innych technologii, takich jak JavaScript, HTML, CSS. Dokumentacja dostępna jest na stronie: dokumentacja. Utworzoną aplikację można w łatwy sposób wdrożyć dzięki chmurze za pomocą kilku kliknięć.
 """)
