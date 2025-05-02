b43a8a41e89a41962fa66894a70cca8e6d55377f
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 00:30:31 +0200

    Update 1. Czym jest Streamlit?.py

--- 1. Czym jest Streamlit?.py
+++ 1. Czym jest Streamlit?.py
@@ -1,1 +1,22 @@
 import streamlit as st
+
+## **Aktywuj nowo utworzone środowisko**
+
+Aby skorzystać ze środowiska condy, które właśnie stworzyliśmy i które nazwaliśmy `stenv`, wprowadź następujące polecenie w linii poleceń:
+
+```bash
+conda activate stenv
+```
+
+## **Zainstaluj bibliotekę Streamlit**
+
+Nadszedł czas, aby wreszcie zainstalować samą bibliotekę `streamlit`:
+```bash
+pip install streamlit
+```
+
+## **Uruchamianie przykładowej aplikacji**
+Aby uruchomić przykładową aplikację (Ilustracja 1), wpisz:
+```bash
+streamlit hello
+```
