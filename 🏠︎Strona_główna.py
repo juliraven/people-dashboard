65a654d9ff11ad62ace06ee961524adc25f10603
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 01:36:39 +0200

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -105,7 +105,7 @@ st.title("Udostępnianie aplikacji")
 st.markdown(
     '''
     <p>
-    Utworzoną aplikację można udostępnić do publicznego użytku przy pomocy a href="https://github.com/" style="color:#66ccff; font-weight:bold;">Githuba</a>. Wystarczy utworzyć konto i repozytorium, które można użyć do udostępnienia aplikacji za pomocą wyżej wspomnianej chmury.
+    Utworzoną aplikację można udostępnić do publicznego użytku przy pomocy <a href="https://github.com/" style="color:#66ccff; font-weight:bold;">Githuba</a>. Wystarczy utworzyć konto i repozytorium, które można użyć do udostępnienia aplikacji za pomocą wyżej wspomnianej chmury.
     </p>
     ''',
     unsafe_allow_html=True
