d1b80cd63f3e1e74b3268df9b7de308aa4bc24d1
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 03:13:22 +0200

    Update 1_🔴Prosta aplikacja.py

--- pages/1_🔴Prosta aplikacja.py
+++ pages/1_🔴Prosta aplikacja.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
+page_bg_img_sidebar = """
+<style>
+[data-testid="stSidebar"] {
+    background: radial-gradient(circle at 51% 40%, #b444fb, #2d035e, #202125); 
+    background-blend-mode: multiply;
+    background-size: cover;
+    overflow: hidden; /* Prevent scrolling */
+}
 
+header[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+body {
+    background-color: #202125; 
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
