9e87f789f386470eefad70e5bf965eababc711f9
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 21:54:24 +0200

    Update 3_🟣Tworzenie dashboardu.py

--- pages/3_🟣Tworzenie dashboardu.py
+++ pages/3_🟣Tworzenie dashboardu.py
@@ -1,12 +1,44 @@
 import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+add_logo('logo.png', height=350)
+
+st.sidebar.markdown(
+    """
+    <style>
+        }
+        [data-testid="stSidebar"] {
+            padding-top: 0px;
+            padding: 10px;
+            font-family: sans-serif;
+            font-size: 18px;
+            width: 150px !important; /* Wymuszenie */
+            min-width: 150px !important;
+            max-width: 150px !important;
+        }
+
+        [data-testid="stSidebarHeader"] {
+            height: 30px;
+            padding: 5px 10px; 
+            margin: 0; 
+            display: flex; 
+            align-items: center;
+            justify-content: center; 
+        }
+        .main {
+            margin-left: 170px;
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 page_bg_img_sidebar = """
 <style>
 [data-testid="stSidebar"] {
-    background: radial-gradient(circle at 51% 50%, #202125, #2d035e, #b444fb); 
+    background: radial-gradient(circle at 51% 40%, #b444fb, #2d035e, #202125); 
     background-blend-mode: multiply;
     background-size: cover;
-    overflow: hidden; 
+    overflow: hidden; /* Prevent scrolling */
 }
 
 header[data-testid="stHeader"] {
@@ -22,4 +54,5 @@ body {
 
 st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
 
+st.title("Tworzenie dashboardu")
 
