25825dbea54fd328c145681644157429c4e159c4
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 22:11:04 +0200

    Update 2_🟣Prosta aplikacja.py

--- pages/2_🟣Prosta aplikacja.py
+++ pages/2_🟣Prosta aplikacja.py
@@ -85,7 +85,7 @@ for _, row in top_movies.iterrows():
 for _, row in top_movies.iterrows():
     st.markdown(
         f"""
-        <div style="background: rgba(255, 255, 255, 0.1);
+        <div style="background: rgba(255, 255, 255, 0.5);
                     backdrop-filter: blur(30px);
                     -webkit-backdrop-filter: blur(30px);
                     border-radius: 10px;
