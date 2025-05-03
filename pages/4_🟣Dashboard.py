76db65a3d89c1441484a3218727fe858cbd1cc6b
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 21:53:55 +0200

    Update 4_🟣Dashboard.py

--- pages/4_🟣Dashboard.py
+++ pages/4_🟣Dashboard.py
@@ -1,1 +1,24 @@
+import streamlit as st
+
+page_bg_img_sidebar = """
+<style>
+[data-testid="stSidebar"] {
+    background: radial-gradient(circle at 51% 50%, #202125, #2d035e, #b444fb); 
+    background-blend-mode: multiply;
+    background-size: cover;
+    overflow: hidden; 
+}
+
+header[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+body {
+    background-color: #202125; 
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
 
