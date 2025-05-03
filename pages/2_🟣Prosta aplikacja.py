7df068db213fa5628221d772cc84e730d02752c1
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 22:13:20 +0200

    Update 2_🟣Prosta aplikacja.py

--- pages/2_🟣Prosta aplikacja.py
+++ pages/2_🟣Prosta aplikacja.py
@@ -86,8 +86,7 @@ for _, row in top_movies.iterrows():
     st.markdown(
         f"""
         <div style="background: rgba(32, 33, 37, 0.2);
-                    backdrop-filter: blur(30px);
-                    -webkit-backdrop-filter: blur(30px);
+                    filter: blur(15px);
                     border-radius: 10px;
                     padding: 15px;
                     margin-bottom: 20px;
