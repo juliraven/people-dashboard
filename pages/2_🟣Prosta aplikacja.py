924c6e32515d367ece78a491c21a64dde0962dd3
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 03:58:59 +0200

    Update 2_🟣Prosta aplikacja.py

--- pages/2_🟣Prosta aplikacja.py
+++ pages/2_🟣Prosta aplikacja.py
@@ -80,10 +80,7 @@ with col2:
                    markers=True, title="Średnia ocena wg roku",
                    labels={"release_year": "Rok", "średnia_ocena": "Średnia ocena"})
     fig2.update_layout(title_x=0.5)
-    fig2.update_traces(
-    line_color="#b444fb",
-    fill='tozeroy',  # <-- to dodaje "poświatę" pod wykresem
-    fillcolor='rgba(180, 68, 251, 0.2)')  # przezroczysty fiolet
+    fig2.update_traces(line_color="#b444fb")
     st.plotly_chart(fig2, use_container_width=True)
 
 st.subheader("⭐ Top filmy wg oceny")
