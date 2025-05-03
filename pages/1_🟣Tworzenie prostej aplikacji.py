e55bf0517a2f9529b54239f89e8513ab667548f0
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 21:02:55 +0200

    Update 1_🟣Tworzenie prostej aplikacji.py

--- pages/1_🟣Tworzenie prostej aplikacji.py
+++ pages/1_🟣Tworzenie prostej aplikacji.py
@@ -1,1 +1,47 @@
+import streamlit as st
 
+page_bg_img_sidebar = """
+<style>
+[data-testid="stSidebar"] {
+    background: radial-gradient(circle at 51% 40%, #b444fb, #2d035e, #202125); 
+    background-blend-mode: multiply;
+    background-size: cover;
+    overflow: hidden; /* Prevent scrolling */
+}
+
+header[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+body {
+    background-color: #202125; 
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
+
+st.title("Tworzenie prostej aplikacji")
+
+st.markdown(
+    '''
+    <p>
+    W celu utworzenia prostej aplikacji zawierającej wizualizacje danych np. zawartych w pliku z rozszerzeniem <code>.csv</code>, należy ten plik umieścić w repozytorium wraz z innymi plikami. Repozytorium będzie miało wtedy następującą strukturę:
+    </p>
+    ''',
+    unsafe_allow_html=True
+)
+
+
+code = '''
+app/
+├── home.py
+└── pages/
+    └── page1.py
+    └── page2.py
+├── requirements.txt
+├── data.csv
+'''
+
+st.code(code, language='python')
