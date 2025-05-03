e6ca1099e9a87493fbff5e490475bcc7e4c30e9a
julia <168024513+juliraven@users.noreply.github.com>
2025-05-03 02:24:16 +0200

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -47,7 +47,7 @@ st.markdown(
 page_bg_img_sidebar = """
 <style>
 [data-testid="stSidebar"] {
-    background: radial-gradient(circle at 51% 45%, #b444fb, #2d035e, #202125); 
+    background: radial-gradient(circle at 51% 30%, #b444fb, #2d035e, #202125); 
     background-blend-mode: multiply;
     background-size: cover;
     overflow: hidden; /* Prevent scrolling */
