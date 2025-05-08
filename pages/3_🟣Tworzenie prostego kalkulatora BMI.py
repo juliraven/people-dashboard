4f7e6ef1d50dc858bcae0ebed861ee46654bd5a5
julia <168024513+juliraven@users.noreply.github.com>
2025-05-08 18:17:38 +0200

    Update 3_🟣Tworzenie prostego kalkulatora BMI.py

--- pages/3_🟣Tworzenie prostego kalkulatora BMI.py
+++ pages/3_🟣Tworzenie prostego kalkulatora BMI.py
@@ -1,1 +1,58 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+add_logo('logo.png', height=350)
+
+st.sidebar.markdown(
+    """
+    <style>
+        }
+        [data-testid="stSidebar"] {
+            padding-top: 0px;
+            padding: 10px;
+            font-family: sans-serif;
+            font-size: 18px;
+            width: 150px !important; /* Wymuszenie */
+            min-width: 150px !important;
+            max-width: 150px !important;
+        }
+
+        [data-testid="stSidebarHeader"] {
+            height: 30px;
+            padding: 5px 10px; 
+            margin: 0; 
+            display: flex; 
+            align-items: center;
+            justify-content: center; 
+        }
+        .main {
+            margin-left: 170px;
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+page_bg_img_sidebar = """
+<style>
+[data-testid="stSidebar"] {
+    background: radial-gradient(circle at 51% 40%, #b444fb, #2d035e, #202125); 
+    background-blend-mode: multiply;
+    background-size: cover;
+    overflow: hidden; /* Prevent scrolling */
+}
+
+header[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+body {
+    background-color: #202125; 
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
+
+st.title("Tworzenie prostego kalkulatora BMI")
 
