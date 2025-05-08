fa98c7645359c1a1dbf00bbb92adbe3876af9671
julia <168024513+juliraven@users.noreply.github.com>
2025-05-08 19:04:06 +0200

    Update 7_🟣Koniec.py

--- pages/7_🟣Koniec.py
+++ pages/7_🟣Koniec.py
@@ -43,7 +43,7 @@ st.markdown(
     }
     .h2-style {
         font-size: 14px; /* Zmniejszenie czcionki dla h2 */
-        color: #202125 !important; /* Zmiana koloru czcionki */
+        color: #b444fb !important; /* Zmiana koloru czcionki */
     }
     </style>
     
