b68bb97c3cfe14fe05516e80d57c309a3fed9603
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 23:25:09 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -235,9 +235,10 @@ with col[1]:
         """
         <div style="border: 2px solid #444; padding: 20px; border-radius: 10px; background-color: #222;">
             <h3 style="color: white;">Populacja Polski</h3>
+            <div style="height: 350px;">
             """, unsafe_allow_html=True)
-    st.plotly_chart(fig2)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.plotly_chart(fig2, use_container_width=True)
+    st.markdown("</div></div>", unsafe_allow_html=True)
     
     st.markdown(f"### Populacja Europy {selected_years[0]} - {selected_years[1]}")
     st.altair_chart(fig1, use_container_width=True)
