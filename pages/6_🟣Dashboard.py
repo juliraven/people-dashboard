eed4079877c721418da95fd417b3d2aed8a0d460
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 19:06:04 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -168,7 +168,7 @@ fig2.add_trace(
     px.scatter_geo(
         pd.DataFrame({"ISO3": [None], "Population": [None], "label": ["Brak danych"]}),
         locationmode="ISO-3",
-        hover_name="Country",
+        hover_name="label",
         text="label"
     ).update_traces(marker=dict(color="white", size=10), showlegend=True, name="Brak danych").data[0]
 )
