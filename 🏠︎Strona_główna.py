c58d55ae796387b8de20d7487b9dc043365744af
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 16:49:21 +0200

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -278,7 +278,7 @@ df = pd.DataFrame({
     'PKB': [150000, 120000, 110000, 130000]
 })
 
-col1, col2 = st.columns([2.5, 3])
+col1, col2 = st.columns([1, 3])
 
 with col1:
     st.subheader("📋 Tabela danych")
