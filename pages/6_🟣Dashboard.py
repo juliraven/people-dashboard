736639e491a32b134478c01fa9a5337fb8f7fed7
julia <168024513+juliraven@users.noreply.github.com>
2025-05-10 00:47:48 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -219,8 +219,12 @@ fig2.update_layout(
 df_map["Population"] = df_map["Population"].astype(int)
 df_map = df_map.sort_values(by="Population", ascending=False)
 
-df_this_year = df_map[df_map["Year"] == selected_year_for_map]
-df_prev_year = df_map[df_map["Year"] == (selected_year_for_map - 1)]
+df4 = df1[df1["Country"].isin(geo.keys())].copy()
+df4["Population"] = pd.to_numeric(df4["Population"], errors="coerce")
+df4 = df4.dropna(subset=["Population"])
+
+df_this_year = df4[df4["Year"] == selected_year_for_map]
+df_prev_year = df4[df4["Year"] == (selected_year_for_map - 1)]
     
 df_diff = df_this_year.merge(df_prev_year, on="Country", suffixes=("_now", "_prev"))
 df_diff["Population_Change"] = df_diff["Population_now"] - df_diff["Population_prev"]
