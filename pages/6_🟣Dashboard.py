201e55c6d146f5fbd85f9aeb209d2b217fb8eb88
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 21:11:04 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -210,7 +210,7 @@ fig2.update_layout(
         yanchor="middle",
         y=0.5,                        
         xanchor="right",
-        x=1.5,                        
+        x=1.2,                        
         font=dict(size=14)
     )
 )
