48a959950d7977b277bb836c4e1d96181b23b6f2
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 17:54:56 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -78,7 +78,7 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     ).properties(width=900).configure_axis(
         labelFontSize=12,
         titleFontSize=12
-    ), height=600
+    )
     return heatmap
 
 fig1 = make_heatmap(
@@ -86,7 +86,7 @@ fig1 = make_heatmap(
     input_y="Year",
     input_x="Country",
     input_color="Population",
-    input_color_theme="purplered"
+    input_color_theme="bluepurple"
 )
 
 st.altair_chart(fig1, use_container_width=True)
