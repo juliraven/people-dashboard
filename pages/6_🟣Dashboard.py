d04f92325207a564afd041a2c7c6ad5d3d6d4709
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 20:59:32 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -75,7 +75,7 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
             title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
         color=alt.Color(f'{input_color}:Q',
                         legend=alt.Legend(title="Liczba ludności"),
-                        scale=alt.Scale(scheme=input_color_theme, reverse=True)),
+                        scale=alt.Scale(scheme=input_color_theme)),
         stroke=alt.value('black'),
         strokeWidth=alt.value(0.25),
     ).properties(width=900).configure_axis(
