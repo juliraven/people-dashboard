2cbec9c4ebe1f592bc6a5f706a5f4c6d237d538b
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 23:33:33 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -230,11 +230,13 @@ df_map["Population"] = df_map["Population"].astype(int)
 df_map = df_map.sort_values(by="Population", ascending=False)
 
 with col[1]:
-    st.markdown(f"### Populacja świata w roku {selected_year_for_map}")
-    st.plotly_chart(fig2, use_container_width=True)
-    
-    st.markdown(f"### Populacja Europy {selected_years[0]} - {selected_years[1]}")
-    st.altair_chart(fig1, use_container_width=True)
+    with st.container():
+        st.markdown(f"### Populacja świata w roku {selected_year_for_map}")
+        st.plotly_chart(fig2, use_container_width=True)
+
+    with st.container():
+        st.markdown(f"### Populacja Europy {selected_years[0]} - {selected_years[1]}")
+        st.altair_chart(fig1, use_container_width=True)
     
 
 with col[2]:
