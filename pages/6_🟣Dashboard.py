8263ea87f526b3420c8a71cb9f5df9e9c4988d12
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 19:23:03 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -161,8 +161,7 @@ fig2.add_trace(
         scope="europe",
         color_discrete_sequence=["#a0a0a0"],  
         hover_name="Country",  
-        hover_data=["Country"],
-        hover_data={"ISO3": False}
+        hover_data={"ISO3": False, "Country": True}
     ).data[0]
 )
 
