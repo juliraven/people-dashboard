478024306095ce9df95c1597eb3fbb7a3e467783
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 22:51:06 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -29,6 +29,7 @@ import plotly.graph_objects as go
 import altair as alt
 
 st.markdown("<h1 style='text-align: center; margin-top: -50px;'>📊 Dashboard ludności świata (2000–2023)</h1>", unsafe_allow_html=True)
+st.markdown('#')
 
 df = pd.read_excel('plik.xlsx')
 
