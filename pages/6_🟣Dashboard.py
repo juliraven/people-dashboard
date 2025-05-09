95f6e3fcee60a14e6a5cf27669f9638a608b173d
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 23:49:08 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -247,7 +247,7 @@ with col[2]:
     with st.container(border=True):
         with st.expander('Żródło danych:', expanded=True):
             st.markdown('<span style="color: purple; font-weight: bold;">World population</span>: '
-    '<a href="https://ourworldindata.org/population-growth" target="_blank">source</a>',
+    '<a href="https://ourworldindata.org/population-growth" target="_blank"></a>',
     unsafe_allow_html=True
 )
 
