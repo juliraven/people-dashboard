e3755e35994fb9471b705f61d78f6ad9ecc3e18b
julia <168024513+juliraven@users.noreply.github.com>
2025-05-09 17:42:21 +0200

    Update 6_🟣Dashboard.py

--- pages/6_🟣Dashboard.py
+++ pages/6_🟣Dashboard.py
@@ -25,6 +25,7 @@ st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import altair as alt
 
 st.title("📊 Dashboard ludności Europy (2015–2024)")
 
@@ -59,6 +60,22 @@ df2 = df1[(df1["Country"].isin(selected_countries)) &
     (df1["Year"] <= selected_years[1])
 ]
 
+def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
+    heatmap = alt.Chart(input_df).mark_rect().encode(
+            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            color=alt.Color(f'max({input_color}):Q',
+                             legend=None,
+                             scale=alt.Scale(scheme=input_color_theme)),
+            stroke=alt.value('black'),
+            strokeWidth=alt.value(0.25),
+        ).properties(width=900
+        ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=12
+        ) 
+    # height=300
+    return heatmap
 
 
 
