6166edfe3907a4dbe932908a2babc92b8fe96679
julia <168024513+juliraven@users.noreply.github.com>
2025-05-10 02:31:22 +0200

    Update 5_🟣Dashboard 1.py

--- pages/5_🟣Dashboard 1.py
+++ pages/5_🟣Dashboard 1.py
@@ -242,7 +242,7 @@ top_loss = df_diff.sort_values("Population_Change").iloc[0]
 st.markdown(
     """
     <style>
-    div[data-testid="stVerticalBlock"]:has(div#gradient_container_marker):not(:has(div#outer_marker)) {
+    div[data-testid="stVerticalBlock"] {
         background: linear-gradient(
             135deg,
             rgba(180, 68, 251, 0.25),
@@ -256,11 +256,10 @@ st.markdown(
             0 0 10px rgba(180, 68, 251, 0.25),
             0 4px 20px rgba(0, 0, 0, 0.3);
         backdrop-filter: blur(14px) brightness(1.1);
-        background-blend-mode: overlay;
         transition: all 0.3s ease-in-out;
     }
 
-    div[data-testid="stVerticalBlock"]:has(div#gradient_container_marker):not(:has(div#outer_marker)):hover {
+    div[data-testid="stVerticalBlock"]:hover {
         transform: translateY(-6px);
         box-shadow:
             0 0 18px rgba(180, 68, 251, 0.4),
@@ -270,6 +269,8 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+
 
 with col[0]:
     styled_container = st.container()
