6088c97e77a055202353f17c4504dd3d1a08fd1a
julia <168024513+juliraven@users.noreply.github.com>
2025-05-11 02:46:55 +0200

    Update 5_🟣Dashboard 1.py

--- pages/5_🟣Dashboard 1.py
+++ pages/5_🟣Dashboard 1.py
@@ -340,7 +340,7 @@ with col[0]:
         with col1:
             st.markdown(
     f"""
-    <div style='border: 2px solid #FF69B4; border-radius: 10px; padding: 10px; height: 150px; text-align: center;'>
+    <div style='border: 2px solid #FF69B4; border-radius: 10px; padding: 10px; height: 170px; text-align: center;'>
         <img src="data:image/jpg;base64,{icon_k}" width="100"><br>
         <span>Kobiety <span style='color: #FF69B4; font-weight: bold;'>{procent_kobiet:.1f}%</span></span>
     </div>
