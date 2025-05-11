cdba1f77e498f0563023e5d614f1f5c49e5a03fa
julia <168024513+juliraven@users.noreply.github.com>
2025-05-11 04:31:52 +0200

    Update 5_🟣Dashboard 1.py

--- pages/5_🟣Dashboard 1.py
+++ pages/5_🟣Dashboard 1.py
@@ -387,7 +387,7 @@ with styled_container:
 )
 
     fig1.update_traces(mode='lines+markers', 
-                  marker=dict(size=4, symbol='circle'), line=dict(width=3))
+                  marker=dict(size=4, symbol='circle'), line=dict(width=2))
 
     fig1.update_layout(
     xaxis=dict(
