0351ece1d179f46717fe76026b5005e694866b95
julia <168024513+juliraven@users.noreply.github.com>
2025-05-11 03:54:55 +0200

    Update 5_🟣Dashboard 1.py

--- pages/5_🟣Dashboard 1.py
+++ pages/5_🟣Dashboard 1.py
@@ -344,7 +344,7 @@ with col[2]:
 
     with styled_container:
         st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
-        with st.expander('Żródło danych:', expanded=False):
+        with st.expander('Żródła danych:', expanded=False):
             st.markdown('<a href="https://ourworldindata.org/population-growth" target="_blank">https://ourworldindata.org/population-growth</a>', unsafe_allow_html=True)
             st.markdown('<a href="https://ourworldindata.org/grapher/population" target="_blank">https://ourworldindata.org/grapher/population</a>', unsafe_allow_html=True)
 
