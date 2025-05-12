e4980d7a0268b3c1e109185bbcb5e14003539415
julia <168024513+juliraven@users.noreply.github.com>
2025-05-13 01:23:51 +0200

    Update 1_🟣Dashboard 1.py

--- pages/1_🟣Dashboard 1.py
+++ pages/1_🟣Dashboard 1.py
@@ -450,6 +450,7 @@ with col22:
         st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
         st.markdown(f"<h3 style='text-align: center; color: white;'>Oczekiwana długość życia</h3>",unsafe_allow_html=True)
 
+        df5 = pd.read_csv('life-expentancy.csv')
        
 
 
