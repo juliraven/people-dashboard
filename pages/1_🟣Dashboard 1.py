add75c52f25ff636109c88ff56dee429a3d432fa
julia <168024513+juliraven@users.noreply.github.com>
2025-05-13 00:30:41 +0200

    Update 1_🟣Dashboard 1.py

--- pages/1_🟣Dashboard 1.py
+++ pages/1_🟣Dashboard 1.py
@@ -12,7 +12,7 @@ section[data-testid="stSidebar"] {
     justify-content: center;   /* Wyśrodkowanie w poziomie */
     flex-direction: column;
     height: 100vh;             /* Wysokość całego widoku */
-    padding-top: 200px;
+    padding-top: 10px;
 }
 
 /* Styl samego wnętrza sidebaru */
