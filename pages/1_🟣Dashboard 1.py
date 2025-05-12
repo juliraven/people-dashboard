8e51ab4c33f3858d3271a05a970afd5268d35446
julia <168024513+juliraven@users.noreply.github.com>
2025-05-13 01:47:11 +0200

    Update 1_🟣Dashboard 1.py

--- pages/1_🟣Dashboard 1.py
+++ pages/1_🟣Dashboard 1.py
@@ -376,6 +376,7 @@ with col[2]:
         with st.expander('Żródła danych:', expanded=False):
             st.markdown('<a href="https://ourworldindata.org/population-growth" target="_blank">https://ourworldindata.org/population-growth</a>', unsafe_allow_html=True)
             st.markdown('<a href="https://ourworldindata.org/grapher/population" target="_blank">https://ourworldindata.org/grapher/population</a>', unsafe_allow_html=True)
+            st.markdown('<a href="https://ourworldindata.org/grapher/deaths-by-age-group" target="_blank">https://ourworldindata.org/grapher/deaths-by-age-group</a>', unsafe_allow_html=True)
 
 col11, col22 = st.columns([2,2])
 
