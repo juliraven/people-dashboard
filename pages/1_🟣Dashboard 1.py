ecdd43679990d90c6d27b10661ad7835ef92b3c0
julia <168024513+juliraven@users.noreply.github.com>
2025-05-13 00:48:24 +0200

    Update 1_🟣Dashboard 1.py

--- pages/1_🟣Dashboard 1.py
+++ pages/1_🟣Dashboard 1.py
@@ -342,7 +342,7 @@ with col[1]:
 
     with styled_container:
         st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
-        st.markdown(f"<h3 style='text-align: center; color: white;'>Populacja świata w roku {selected_year_for_map}</h3>",unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center; color: white;'>Populacja świata w {selected_year_for_map}</h3>",unsafe_allow_html=True)
         st.plotly_chart(fig2, use_container_width=True, config={"displayModeBar": False})
 
 with col[2]:
