dada2e1b0252fc70c594a3c6865f9b327cd11e5c
julia <168024513+juliraven@users.noreply.github.com>
2025-05-13 02:43:31 +0200

    Update 1_🟣Dashboard 1.py

--- pages/1_🟣Dashboard 1.py
+++ pages/1_🟣Dashboard 1.py
@@ -384,6 +384,9 @@ de = pd.read_csv('deaths-by-age-group.csv')
 df_long = de.melt(id_vars=["Entity", "Year"], value_vars=["100+", "90-99", "80-89", "70-79", "60-69", "50-59", "40-49", "30-39", "20-29", "10-19", "0-9"],
                   var_name="Age_group", value_name="Deaths")
 
+age_order = ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90-99", "100+"]
+df_long["Age_group"] = pd.Categorical(df_long["Age_group"], categories=age_order, ordered=True)
+
 obszary = de["Entity"].unique().tolist()
 wybrane_obszary = st.selectbox('Wybierz obszary:', obszary, index=obszary.index('World'))
 wybrane_obszary = [wybrane_obszary]
