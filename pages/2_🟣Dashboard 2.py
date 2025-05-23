7e40ff6c359c896f948e87589cc2c91611e38656
julia <168024513+juliraven@users.noreply.github.com>
2025-05-23 18:11:58 +0200

    Update 2_🟣Dashboard 2.py

--- pages/2_🟣Dashboard 2.py
+++ pages/2_🟣Dashboard 2.py
@@ -542,6 +542,8 @@ with tab2:
         st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
         st.markdown(f"<h3 style='text-align: center; color: white;'>Oczekiwana długość życia: kobiety vs mężczyźni w 2023</h3>",unsafe_allow_html=True)
         st.plotly_chart(fig, config={"displayModeBar": False})
+
+    
        
 
 with tab3:
