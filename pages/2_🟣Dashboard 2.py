39bd31e657a3c148d33629db57a699b6501f3748
julia <168024513+juliraven@users.noreply.github.com>
2025-05-25 21:49:23 +0200

    Update 2_🟣Dashboard 2.py

--- pages/2_🟣Dashboard 2.py
+++ pages/2_🟣Dashboard 2.py
@@ -802,9 +802,9 @@ with tab3:
 
     styled_container = st.container()
     st.markdown("<div id='outer_marker'></div>", unsafe_allow_html=True)
-        with styled_container:
-            st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
-            st.components.v1.html(html,
+    with styled_container:
+        st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
+        st.components.v1.html(html,
     height=750,
     scrolling=False
 )
